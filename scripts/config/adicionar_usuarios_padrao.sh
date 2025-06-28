9c1d6c35cbdd5744506cd7bf7b5e7d9185017f43
Roger Agostinho <167251142+rogeragostinho@users.noreply.github.com>
2025-06-28 18:42:50 +0100

    Update adicionar_usuarios_padrao.sh

--- scripts/config/adicionar_usuarios_padrao.sh
+++ scripts/config/adicionar_usuarios_padrao.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Usuário para grupo recepcao
-sudo useradd -m -G angolacars, angolacars_recepcao recep1
+sudo useradd -m -G angolacars,angolacars_recepcao recep1
 echo "recep1:senha123" | sudo chpasswd
 
 # Usuário para grupo vendas
-sudo useradd -m -G angolacars, angolacars_vendas vendedor1
+sudo useradd -m -G angolacars,angolacars_vendas vendedor1
 echo "vendedor1:senha123" | sudo chpasswd
 
 # Descobrir usuário atual
 meu_user=$(whoami)
 
 # Adicionar ao grupo admin
-sudo usermod -aG angolacars, angolacars_admin "$meu_user"
+sudo usermod -aG angolacars,angolacars_admin "$meu_user"
