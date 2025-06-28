9b7be4a8556dd3ea04695ad133fd31121f2fdf86
Roger Agostinho <167251142+rogeragostinho@users.noreply.github.com>
2025-06-28 18:27:26 +0100

    Update criar_grupos_usuarios.sh

--- scripts/config/criar_grupos_usuarios.sh
+++ scripts/config/criar_grupos_usuarios.sh
@@ -2,5 +2,5 @@
 
 sudo getent group angolacars           || sudo groupadd angolacars
 sudo getent group angolacars_admin     || sudo groupadd angolacars_admin
-sudo getent group angolacars_recepcao  || sudo angolacars_recepcao recepcao
-sudo getent group angolacars_vendas    || sudo angolacars_vendas vendas
+sudo getent group angolacars_recepcao  || sudo groupadd angolacars_recepcao
+sudo getent group angolacars_vendas    || sudo groupadd angolacars_vendas
