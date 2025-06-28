63719b7510b90929d16f742dd798c0e92d6ba866
Roger Agostinho <167251142+rogeragostinho@users.noreply.github.com>
2025-06-28 18:08:12 +0100

    Update index.sh

--- scripts/admin/index.sh
+++ scripts/admin/index.sh
@@ -3,11 +3,4 @@
 #define o diretorio do prórpio script
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-echo "[1] Criar novo usuário"
-
-read -p "> " opcao
-
-case $opcao in
-    1) bash $BASE_DIR/menu_criar_novo_usuario.sh
-    *) ;;
-esac
+bash $BASE_DIR/menu_principal.sh
