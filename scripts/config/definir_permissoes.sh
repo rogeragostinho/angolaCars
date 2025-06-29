2e9d2da61c79e62486f6280d908772b1e1734eff
Roger Agostinho <167251142+rogeragostinho@users.noreply.github.com>
2025-06-29 17:26:02 +0100

    Update definir_permissoes.sh

--- scripts/config/definir_permissoes.sh
+++ scripts/config/definir_permissoes.sh
@@ -21,5 +21,6 @@ sudo chmod -R 777 /var/opt/angolacars
 #sudo chmod 777 /var/opt/angolacars
 #sudo chmod g+s /var/opt/angolacars
 
-sudo chmod -R 775 /var/opt/angolacars/logs
+sudo chown -R :angolacars /var/opt/angolacars/logs
+sudo chmod -R 777 /var/opt/angolacars/logs
 
