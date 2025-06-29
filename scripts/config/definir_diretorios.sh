5c0e872f2cff7c2012705b1e73f061a1c092d1e0
Roger Agostinho <167251142+rogeragostinho@users.noreply.github.com>
2025-06-29 15:43:12 +0100

    Update definir_diretorios.sh

--- scripts/config/definir_diretorios.sh
+++ scripts/config/definir_diretorios.sh
@@ -2,9 +2,11 @@
 
 sudo mkdir -p /var/opt/angolacars/logs
 sudo mkdir -p /var/opt/angolacars/vendas
+sudo mkdir -p /var/opt/angolacars/vendas/logs
 sudo mkdir -p /var/opt/angolacars/clientes_interessados
 sudo mkdir -p /var/opt/angolacars/dados
 
 #temporario
-sudo touch /var/opt/angolacars/vendas/vendas.log
+#sudo touch /var/opt/angolacars/vendas/vendas.log
 sudo touch /var/opt/angolacars/dados/carros.txt
+sudo touch /var/opt/angolacars/vendas/logs/vendas.log
